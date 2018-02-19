ef2c802079035a4623172a778870ea1c7100a91e
Matt Brucker <mpbrucker@gmail.com>
2018-02-19 10:02:22 -0500

    Made box moving work properly.

--- app/src/index.js
+++ app/src/index.js
@@ -2,38 +2,59 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
 
-class App extends React.Component {
+class DraggableBox extends React.Component {
 
     constructor(props) {
       super(props);
       this.state = {x: 0, y: 0, draggable: false, downX: 0, downY: 0, elemX: 0, elemY: 0};
     }
 
-    _onMouseMove(e) {
-      this.setState({x: e.screenX, y: e.screenY});
+    componentDidMount(props, state) {
+      // We have to add document listeners so it will update pos even when
+      document.addEventListener('mousemove', this.mouseMove.bind(this))
+      document.addEventListener('mouseup', this.mouseUp.bind(this))
+    }
+
+    mouseMove(e) {
+      this.setState({x: e.screenX, y: e.screenY})
       if (this.state.draggable) {
-          this.setState({elemX: this.state.x + this.state.downX, elemY: this.state.y + this.state.downY})
+        this.setState({elemX: this.state.x + this.state.downX, elemY: this.state.y + this.state.downY})
       }
     }
 
-    _onMouseDown(e) {
+    mouseDown(e) {
       this.setState({draggable: true, downX: this.state.elemX - e.screenX, downY: this.state.elemY - e.screenY});
     }
 
-    _onMouseUp(e) {
+    mouseUp(e) {
       this.setState({draggable: false});
     }
 
-    render() {
+    mouseExit(e) {
+      this.style.cursor = 'default'
+    }
+
+    mouseEnter(e) {
+      this.style.cursor = 'move'
+    }
+
+    getBoxStyle() {
       var boxStyle = {
         backgroundColor: 'red',
         left: this.state.elemX + 'px',
         top: this.state.elemY + 'px'
       };
+      return boxStyle;
+    }
+
+    render() {
       return (
-      <div onMouseMove={this._onMouseMove.bind(this)} className={'Screen'}>
-        <div onMouseUp={this._onMouseUp.bind(this)} onMouseDown={this._onMouseDown.bind(this)}  className={'Box'} style={boxStyle}></div>
-      </div>
+        <div
+        onMouseDown={this.mouseDown.bind(this)}
+        onMouseEnter={this.mouseEnter.bind(this)}
+        onMouseExit = {this.mouseExit.bind(this)}
+        className={'Box'}
+        style={this.getBoxStyle()}></div>
       )
     }
 
@@ -41,4 +62,4 @@ class App extends React.Component {
 
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<DraggableBox />, document.getElementById('root'));
